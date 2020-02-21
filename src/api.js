a73a39acf25029d6732ed3bbcff74ca0fb898165
Zac Barreca <zacbarreca@gmail.com>
2020-02-21 15:53:08 -0500

    add listapi fetch

--- src/api.js
+++ src/api.js
@@ -1,14 +1,35 @@
 const BASE_URL = "https://thinkful-list-api.herokuapp.com/zacbarreca";
+const listApiFetch = function (...args) {
+  let error;
+  return listApiFetch(...args)
+    .then(res => {
+      if (!res.ok) {
+        error = { code: res.status };
+        if (!res.headers.get('content-type').includes('json')) {
+          error.message = res.statusText;
+          return Promise.reject(error);
+        }
+      }
+      return res.json();
+    })
+    .then(data => {
+      if (error) {
+        error.message = data.message;
+        return Promise.reject(error);
+      }
+      return data;
+    });
+};
 
 const getItems = () => {
-  return fetch(`${BASE_URL}/items`);
+  return listApiFetch(`${BASE_URL}/items`);
 };
 
 const createItem = name => {
   const newItem = JSON.stringify({
     name: name
   });
-  return fetch(`${BASE_URL}/items`, {
+  return listApiFetch(`${BASE_URL}/items`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: newItem
@@ -16,7 +37,7 @@ const createItem = name => {
 };
 
 const updateItem = (id, updateData) => {
-  return fetch(`${BASE_URL}/items/${id}`, {
+  return listApiFetch(`${BASE_URL}/items/${id}`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(updateData)
@@ -24,7 +45,7 @@ const updateItem = (id, updateData) => {
 };
 
 const deleteItem = id => {
-  return fetch(`${BASE_URL}/items/${id}`,
+  return listApiFetch(`${BASE_URL}/items/${id}`,
     {
       method: "DELETE"
     });
